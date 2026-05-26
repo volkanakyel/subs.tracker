8877178e3394a811a7b01bc89bd1404c3a2159ca
Volkan Akyel <volkan@uikielle.com>
2026-05-26 19:52:21 +0200

    style: update layout and class names in index page for improved aesthetics and consistency

--- pages/index.vue
+++ pages/index.vue
@@ -1,59 +1,72 @@
 <template>
-  <div class="min-h-screen bg-[#f8f9fb] antialiased font-sans">
+  <div class="min-h-screen bg-white antialiased font-sans">
     <DashboardLoader @done="isLoaded = true" />
 
     <template v-if="isLoaded">
       <DashboardSidebar />
 
-      <div class="lg:ml-[240px] min-h-screen pb-24 lg:pb-0">
+      <div class="lg:ml-[260px] min-h-screen pb-24 lg:pb-0">
         <DashboardTopbar />
 
-        <main class="max-w-[1240px] mx-auto px-5 sm:px-7 lg:px-10 py-6 lg:py-8 space-y-5">
-          <section class="grid grid-cols-12 gap-5">
-            <MotionItem :delay="0.06" class="col-span-12 lg:col-span-8 h-full">
-              <NetWorthCard />
-            </MotionItem>
-
-            <div class="col-span-12 lg:col-span-4 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-1 lg:grid-rows-3 gap-5">
-              <MotionItem
+        <main>
+          <!-- Quick stats banner -->
+          <MotionItem :delay="0.06">
+            <div class="grid grid-cols-1 sm:grid-cols-3 divide-y sm:divide-y-0 sm:divide-x divide-black/[0.07] border-b border-black/[0.07]">
+              <QuickStatCard
                 v-for="(s, i) in quickStats"
                 :key="s.label"
-                :delay="0.14 + i * 0.06"
-                :x="12"
-                :y="0"
-                class="h-full"
-              >
-                <QuickStatCard v-bind="s" />
-              </MotionItem>
+                v-bind="s"
+                :index="i + 1"
+              />
             </div>
+          </MotionItem>
+
+          <!-- Net worth -->
+          <MotionItem :delay="0.16">
+            <section class="px-6 sm:px-8 lg:px-10 py-9 border-b border-black/[0.07]">
+              <NetWorthCard />
+            </section>
+          </MotionItem>
+
+          <!-- Spending + Investments -->
+          <section class="grid lg:grid-cols-[1.4fr_1fr] divide-y lg:divide-y-0 lg:divide-x divide-black/[0.07] border-b border-black/[0.07]">
+            <MotionItem :delay="0.30">
+              <div class="px-6 sm:px-8 lg:px-10 py-9 h-full">
+                <SpendingCard />
+              </div>
+            </MotionItem>
+            <MotionItem :delay="0.38">
+              <div class="px-6 sm:px-8 lg:px-10 py-9 h-full">
+                <InvestmentsCard />
+              </div>
+            </MotionItem>
           </section>
 
-          <section class="grid grid-cols-12 gap-5">
-            <MotionItem :delay="0.34" class="col-span-12 lg:col-span-7 h-full">
-              <SpendingCard />
+          <!-- Goals / Subscriptions / Calendar -->
+          <section class="grid md:grid-cols-2 xl:grid-cols-3 divide-y md:divide-y-0 md:divide-x divide-black/[0.07] border-b border-black/[0.07]">
+            <MotionItem :delay="0.46">
+              <div class="px-6 sm:px-8 lg:px-10 py-9 h-full">
+                <GoalsCard />
+              </div>
             </MotionItem>
-            <MotionItem :delay="0.40" class="col-span-12 lg:col-span-5 h-full">
-              <InvestmentsCard />
+            <MotionItem :delay="0.52">
+              <div class="px-6 sm:px-8 lg:px-10 py-9 h-full">
+                <SubscriptionsCard />
+              </div>
             </MotionItem>
-          </section>
-
-          <section class="grid grid-cols-12 gap-5">
-            <MotionItem :delay="0.46" class="col-span-12 sm:col-span-6 xl:col-span-4 h-full">
-              <GoalsCard />
-            </MotionItem>
-            <MotionItem :delay="0.52" class="col-span-12 sm:col-span-6 xl:col-span-4 h-full">
-              <SubscriptionsCard />
-            </MotionItem>
-            <MotionItem :delay="0.58" class="col-span-12 xl:col-span-4 h-full">
-              <CalendarCard />
+            <MotionItem :delay="0.58">
+              <div class="px-6 sm:px-8 lg:px-10 py-9 h-full md:col-span-2 xl:col-span-1">
+                <CalendarCard />
+              </div>
             </MotionItem>
           </section>
 
-          <section>
-            <MotionItem :delay="0.64">
+          <!-- Transactions -->
+          <MotionItem :delay="0.64">
+            <section class="px-6 sm:px-8 lg:px-10 py-9">
               <TransactionsCard />
-            </MotionItem>
-          </section>
+            </section>
+          </MotionItem>
         </main>
       </div>
     </template>
